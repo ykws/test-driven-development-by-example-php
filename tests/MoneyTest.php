5c2ee5037b565820d6b7d0d33728144bc6c196f1
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-11-22 09:44:18 +0900

    Add test `Dollar` side effect

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -10,5 +10,7 @@ final class MoneyTest extends TestCase
     $five = new Dollar(5);
     $five->times(2);
     $this->assertEquals(10, $five->amount);
+    $five->times(3);
+    $this->assertEquals(15, $five->amount);
   }
 }
