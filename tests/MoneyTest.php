ee1776803627e4e61df377038d3a38883ab10aa9
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-11-22 11:20:55 +0900

    Add test Franc equality

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -17,9 +17,12 @@ final class MoneyTest extends TestCase
   
   public function testEquality(): void
   {
-    $five = new Dollar(5);
-    $this->assertTrue($five->equals(new Dollar(5)));
-    $this->assertFalse($five->equals(new Dollar(6)));
+    $dollar = new Dollar(5);
+    $this->assertTrue($dollar->equals(new Dollar(5)));
+    $this->assertFalse($dollar->equals(new Dollar(6)));
+    $franc = new Franc(5);
+    $this->assertTrue($franc->equals(new Franc(5)));
+    $this->assertFalse($franc->equals(new Franc(6)));
   }
   
   public function testFrancMultiplication(): void
