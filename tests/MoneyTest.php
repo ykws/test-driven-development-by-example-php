61ad2db19ba3b300f57ed09d8940fe0c9c6a4857
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-11-22 17:19:58 +0900

    Add test sum times

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -96,4 +96,16 @@ final class MoneyTest extends TestCase
     $result = $bank->reduce($sum, 'USD');
     $this->assertEquals(Money::dollar(15), $result);
   }
+  
+  public function testSumTimes(): void
+  {
+    $fiveBucks = Money::dollar(5);
+    $tenFrancs = Money::franc(10);
+    $bank = new Bank();
+    $bank->addRate('CHF', 'USD', 2);
+    $sum = new Sum($fiveBucks, $tenFrancs);
+    $sum = $sum->times(2);
+    $result = $bank->reduce($sum, 'USD');
+    $this->assertEquals(Money::dollar(20), $result);
+  }
 }
