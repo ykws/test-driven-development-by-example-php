546e82f572df7bc660e7d0ef1aa7749f13a8b91b
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-11-22 11:04:20 +0900

    Add test Franc multiplication

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -18,4 +18,11 @@ final class MoneyTest extends TestCase
     $this->assertTrue($five->equals(new Dollar(5)));
     $this->assertFalse($five->equals(new Dollar(6)));
   }
+  
+  public function testFrancMultiplication(): void
+  {
+    $five = new Franc(5);
+    $this->assertEquals(new Franc(10), $five->times(2));
+    $this->assertEquals(new Franc(15), $five->times(3));
+  }
 }
