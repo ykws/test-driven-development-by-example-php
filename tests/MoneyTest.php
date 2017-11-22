fa7afbe037d40234efc174dd784aefbbb79b0219
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-11-22 17:02:02 +0900

    Add test sum plus money

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -84,4 +84,16 @@ final class MoneyTest extends TestCase
     $result = $bank->reduce($fiveBucks->plus($tenFrancs), 'USD');
     $this->assertEquals(Money::dollar(10), $result);
   }
+  
+  public function testSumPlusMoney(): void
+  {
+    $fiveBucks = Money::dollar(5);
+    $tenFrancs = Money::franc(10);
+    $bank = new Bank();
+    $bank->addRate('CHF', 'USD', 2);
+    $sum = new Sum($fiveBucks, $tenFrancs);
+    $sum->plus($fiveBucks);
+    $result = $bank->reduce($sum, 'USD');
+    $this->assertEquals(Money::dollar(15), $result);
+  }
 }
